32d699e37aa43d24e9baa357281cf153a34f7f5d
Daniel Tamas <daniriian@gmail.com>
2020-04-26 22:00:35 +0300

    added ProductCard component and start using it inside home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,8 +2,9 @@ import React from "react";
 import Layout from "../layouts/index";
 import { graphql, Link } from "gatsby";
 import { Carousel } from "react-bootstrap";
-import Img from "gatsby-image";
+
 import BgImage from "../components/BgImage/BgImage";
+import ProductCard from "../components/ProductCard/ProductCard";
 
 import "./index.scss";
 
@@ -12,57 +13,40 @@ export default ({ data }) => {
   return (
     <Layout>
       <Carousel>
-        <Carousel.Item>
-          <BgImage
-            fluid={data.allFile.edges[0].node.childImageSharp.fluid}
-            height="400px"
-            mobileHeight="200px"
-            overlayColor="#0400ff54"
-          />
-        </Carousel.Item>
-        <Carousel.Item>
-          <BgImage
-            fluid={data.allFile.edges[1].node.childImageSharp.fluid}
-            height="400px"
-            mobileHeight="200px"
-            overlayColor="#0400ff54"
-          />
-          <Carousel.Caption>
-            <h3>Second slide label</h3>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-          </Carousel.Caption>
-        </Carousel.Item>
-        <Carousel.Item>
-          <BgImage
-            fluid={data.allFile.edges[2].node.childImageSharp.fluid}
-            height="400px"
-            mobileHeight="200px"
-            overlayColor="#0400ff54"
-          />
-
-          <Carousel.Caption>
-            <h3>Third slide label</h3>
-            <p>
-              Praesent commodo cursus magna, vel scelerisque nisl consectetur.
-            </p>
-          </Carousel.Caption>
-        </Carousel.Item>
+        {data.allFile.edges.map((edge, index) => (
+          <Carousel.Item key={index}>
+            <BgImage
+              fluid={edge.node.childImageSharp.fluid}
+              height="400px"
+              mobileHeight="200px"
+              overlayColor="#0400ff54"
+            />
+            <Carousel.Caption>
+              <h3>Second slide label</h3>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+            </Carousel.Caption>
+          </Carousel.Item>
+        ))}
       </Carousel>
-      <h1>This is Online Medical's Home Page</h1>
-      <p>This is the Home Page</p>
-      {edges.map(({ node }) => (
-        <div key={node.id}>
-          <Link to={node.frontmatter.path}>
-            <h3>titlu: {node.frontmatter.title}</h3>
-          </Link>
-          {node.frontmatter.tags.map((tag, index) => (
-            <span key={index}>{tag}, </span>
-          ))}
+      <h1>Welcome to Online Medical's Shop</h1>
+      <section className="product-section">
+        <div className="container">
+          {edges.map(({ node }) => (
+            <div key={node.id}>
+              <ProductCard cardTitle={node.frontmatter.title} />
+              <Link to={node.frontmatter.path}>
+                <h3>titlu: {node.frontmatter.title}</h3>
+              </Link>
+              {node.frontmatter.tags.map((tag, index) => (
+                <span key={index}>{tag}, </span>
+              ))}
 
-          <p>Taguri {node.frontmatter.tags}</p>
-          <p>Excerpt: {node.excerpt}</p>
+              <p>Taguri {node.frontmatter.tags}</p>
+              <p>Excerpt: {node.excerpt}</p>
+            </div>
+          ))}
         </div>
-      ))}
+      </section>
     </Layout>
   );
 };
